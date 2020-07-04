8477d21bfe211f4674a4a31310e903ea9594969d
jdraxler@gmail.com <jdraxler@gmail.com>
2020-07-03 20:37:26 -0400

    Updates

---
+++ precache-manifest.452fb40de1b0975bde4089aac7c039cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78ee247441efcf32087eb503d6a72a3",
+    "url": "/monster-markdown/index.html"
+  },
+  {
+    "revision": "14bf7d6373b6ef8444c3",
+    "url": "/monster-markdown/static/css/main.61364164.chunk.css"
+  },
+  {
+    "revision": "c8ee065eab4150e8ea86",
+    "url": "/monster-markdown/static/js/2.7a6df85f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/monster-markdown/static/js/2.7a6df85f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14bf7d6373b6ef8444c3",
+    "url": "/monster-markdown/static/js/main.06a5adc8.chunk.js"
+  },
+  {
+    "revision": "8b52d69bcc1d1495d492",
+    "url": "/monster-markdown/static/js/runtime-main.4c1f57e5.js"
+  }
+]);
